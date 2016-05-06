941431766581e55fdffc339360684c38074d9df4
Adrian Perez Lopez <a.przlopez@gmail.com>
2016-05-06 20:24:25 +0200

    Modifico la forma de crear el LOG a la general, poniéndolo como variable.

--- app/src/main/java/factory/novas/gueder/MainActivity.java
+++ app/src/main/java/factory/novas/gueder/MainActivity.java
@@ -6,13 +6,15 @@ import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+    
     @Override //Esto es una anotación, indica que implementamos un método que ya existe.
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //Super, para llamar a la implementación de lo que ya hacía el padre. Llamamos para que se ejecute el código, además añadimos más código.
         setContentView(R.layout.activity_main); //Carga su interfaz de activity_main.
 
         //Muestro mi primero LOG, V de verbose.
-        Log.v("TAG", "ola k ase?");
+        Log.v(MainActivity.TAG, "ola k ase?");
 
     }
 }
